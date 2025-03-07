65f5fc2ab6372dc53905affa31163420dda5f742
Semih <semihkaytaz@gmail.com>
2025-03-07 19:59:10 +0300

    Add files via upload

---
+++ Portföy Site/scripts.js
@@ -0,0 +1,21 @@
+const toggleSwitch = document.getElementById('theme-toggle');
+const body = document.body;
+
+// Kullanıcı tercihi localStorage'dan yükleniyor
+const savedTheme = localStorage.getItem('theme');
+if (savedTheme === 'dark-mode') {
+    body.classList.add('dark-mode');
+    toggleSwitch.checked = true;
+}
+
+toggleSwitch.addEventListener('change', () => {
+    if (toggleSwitch.checked) {
+        body.classList.remove('light-mode');
+        body.classList.add('dark-mode');
+        localStorage.setItem('theme', 'dark-mode');
+    } else {
+        body.classList.remove('dark-mode');
+        body.classList.add('light-mode');
+        localStorage.setItem('theme', 'light-mode');
+    }
+});
